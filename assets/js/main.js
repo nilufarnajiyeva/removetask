718e9c70ed62623470d6ae8da38a9061b54e46fb
Nilufar Najiyeva <127186140+nilufarnajiyeva@users.noreply.github.com>
2023-04-26 20:26:08 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+let elements={
+    arr:["Zehra","adami","eseb eleyir","canimdi"],
+    remove : function (){
+        this.arr.length--
+    }
+}
+elements.remove()
+console.log(elements.arr);
+
